68f9b359965fd4ba1151452dc1467ee838c316bc
domemodox <molnardome7@gmail.com>
2023-10-04 15:55:33 +0200

    forditva -=

--- src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
+++ src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
@@ -41,7 +41,7 @@ Random generator = new Random();
 
     if (r >= FAILURE_RATE) {
       // successful firing
-      this.torpedoCount =- numberOfTorpedos;
+      this.torpedoCount -= numberOfTorpedos;
       success = true;
     } else {
       // simulated failure
